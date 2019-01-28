2a77581de143fc399a17ec04f111a1f4e94b3c51
周飞 <1174977961@qq.com>
2019-01-28 11:18:55 +0800

    other:整理代码

--- iOS_KnowledgeStructure/定位功能/LocationManager.m
+++ iOS_KnowledgeStructure/定位功能/LocationManager.m
@@ -12,11 +12,9 @@
 
 @interface LocationManager () <CLLocationManagerDelegate>
 @property (nonatomic, strong) CLLocationManager *locationManager;
-
 @property (nonatomic, strong) NSNumber *longitude;//经度
 @property (nonatomic, strong) NSNumber *latitude;
-
-@property (nonatomic, strong) CLGeocoder *geocoder;//反向地理编码，地址转经纬度
+@property (nonatomic, strong) CLGeocoder *geocoder;//反向地理编码，地址转经纬度/经纬度转地址
 @property (nonatomic, copy) LocationBlock block;
 @end
 
@@ -30,7 +28,6 @@ SingletonM(LocationManager)
     }
     return self;
 }
-
 
 - (CLLocationManager *)locationManager {
     if (!_locationManager) {
@@ -53,7 +50,6 @@ SingletonM(LocationManager)
         NSLog(@"requestAlwaysAuthorization....");
         [self.locationManager requestAlwaysAuthorization];
     }
-    
     [self.locationManager startUpdatingLocation];
 }
 
@@ -62,11 +58,9 @@ SingletonM(LocationManager)
     [self startLocation];
 }
 
-
 #pragma mark - CLLocationManagerDelegate
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
     NSLog(@"locations:%@",locations);
-    
     CLLocation *locat = [locations lastObject];
     CLLocationCoordinate2D coordinate = locat.coordinate;
     NSLog(@"经度：%f, 纬度：%f",coordinate.longitude, coordinate.latitude);
@@ -96,80 +90,46 @@ SingletonM(LocationManager)
     [self.geocoder geocodeAddressString:address completionHandler:^(NSArray *placemarks, NSError *error) {
         StrongSelf
         //取得第一个地标，地标中存储了详细的地址信息，注意：一个地名可能搜索出多个地址
-        
         CLPlacemark *placemark=[placemarks firstObject];
-        
-        
-        
         CLLocation *location=placemark.location;//位置
-        
         CLRegion *region=placemark.region;//区域
-        
         NSDictionary *addressDic= placemark.addressDictionary;//详细地址信息字典,包含以下部分信息
-        
-        //        NSString *name=placemark.name;//地名
-        
-        //        NSString *thoroughfare=placemark.thoroughfare;//街道
-        
-        //        NSString *subThoroughfare=placemark.subThoroughfare; //街道相关信息，例如门牌等
-        
-        //        NSString *locality=placemark.locality; // 城市
-        
-        //        NSString *subLocality=placemark.subLocality; // 城市相关信息，例如标志性建筑
-        
-        //        NSString *administrativeArea=placemark.administrativeArea; // 州
-        
-        //        NSString *subAdministrativeArea=placemark.subAdministrativeArea; //其他行政区域信息
-        
-        //        NSString *postalCode=placemark.postalCode; //邮编
-        
-        //        NSString *ISOcountryCode=placemark.ISOcountryCode; //国家编码
-        
-        //        NSString *country=placemark.country; //国家
-        
-        //        NSString *inlandWater=placemark.inlandWater; //水源、湖泊
-        
-        //        NSString *ocean=placemark.ocean; // 海洋
-        
-        //        NSArray *areasOfInterest=placemark.areasOfInterest; //关联的或利益相关的地标
-        
+//        NSString *name=placemark.name;//地名
+//        NSString *thoroughfare=placemark.thoroughfare;//街道
+//        NSString *subThoroughfare=placemark.subThoroughfare; //街道相关信息，例如门牌等
+//        NSString *locality=placemark.locality; // 城市
+//        NSString *subLocality=placemark.subLocality; // 城市相关信息，例如标志性建筑
+//        NSString *administrativeArea=placemark.administrativeArea; // 州
+//        NSString *subAdministrativeArea=placemark.subAdministrativeArea; //其他行政区域信息
+//        NSString *postalCode=placemark.postalCode; //邮编
+//        NSString *ISOcountryCode=placemark.ISOcountryCode; //国家编码
+//        NSString *country=placemark.country; //国家
+//        NSString *inlandWater=placemark.inlandWater; //水源、湖泊
+//        NSString *ocean=placemark.ocean; // 海洋
+//        NSArray *areasOfInterest=placemark.areasOfInterest; //关联的或利益相关的地标
         NSLog(@"位置:%@,区域:%@,详细信息:%@",location,region,addressDic);
-        
-        
-        
+
         CLLocationDegrees latitude=location.coordinate.latitude;
-        
         CLLocationDegrees longitude=location.coordinate.longitude;
-        
-        
-        
         NSLog(@"纬度-->%lf,经度-->%lf",latitude,longitude);
-        
-        
-        
         //传给接口的纬度和经度
-        
         self.latitude=[NSString stringWithFormat:@"%lf",latitude];
-        
         self.longitude=[NSString stringWithFormat:@"%lf",longitude];
-        
-        
     }];
     
 }
 
 //根据经纬度转地址
 -(NSString*)latitudeStr:(NSNumber*)Latitude LongitudeStr:(NSNumber*)Longitude {
-    static NSString* addstroing =@"";
+    static NSString *addstroing = @"";
 
-    float a = [Latitude floatValue];
+    float a = [Latitude floatValue];
     float b = [Longitude floatValue];
     CLLocation* location = [[CLLocation alloc] initWithLatitude:a longitude:b];
     //位置反编码, IOS5.0之后CL
     
     [self.geocoder reverseGeocodeLocation:location completionHandler:^(NSArray*placemarks,NSError*error) {
-        
-        for(CLPlacemark*placemark in placemarks) {
+        for(CLPlacemark *placemark in placemarks) {
             //CLPlacemark 地标
 //            NSLog(@"位置:%@", placemark.name);
 //            NSLog(@"街道:%@", placemark.thoroughfare);
@@ -188,6 +148,4 @@ SingletonM(LocationManager)
     NSLog(@"地址：%@",addstroing);
     return addstroing;
 }
-    
-    
 @end
