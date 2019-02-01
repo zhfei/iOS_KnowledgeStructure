f3780bf11c167ac7a9038c63dbb0f1d5c742d551
周飞 <1174977961@qq.com>
2019-02-01 15:06:18 +0800

    feature:添加timer例子

--- iOS_KnowledgeStructure/RunLoop点滴/RunLoopViewController.m
+++ iOS_KnowledgeStructure/RunLoop点滴/RunLoopViewController.m
@@ -19,14 +19,16 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)test1 {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSTimer *timer = [NSTimer timerWithTimeInterval:0.5 target:self selector:@selector(timerFire) userInfo:nil repeats:YES];
+        [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+        [[NSRunLoop currentRunLoop] runMode:NSRunLoopCommonModes beforeDate:[NSDate distantFuture]];
+    });
 }
-*/
+
+- (void)timerFire {
+    NSLog(@"mode:%@",[[NSRunLoop currentRunLoop] currentMode]);
+}
 
 @end
