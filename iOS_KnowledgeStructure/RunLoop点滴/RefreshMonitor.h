9054f7a43b7237d52834fef1680ab5f78ed2ba2a
周飞 <1174977961@qq.com>
2019-02-08 23:48:08 +0800

    add:天下主线程卡屏检测工具类

---
+++ iOS_KnowledgeStructure/RunLoop点滴/RefreshMonitor.h
@@ -0,0 +1,18 @@
+//
+//  RefreshMonitor.h
+//  iOS_KnowledgeStructure
+//
+//  Created by 周飞 on 2019/2/8.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RefreshMonitor : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS_KnowledgeStructure/RunLoop点滴/RefreshMonitor.m
@@ -0,0 +1,13 @@
+//
+//  RefreshMonitor.m
+//  iOS_KnowledgeStructure
+//
+//  Created by 周飞 on 2019/2/8.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import "RefreshMonitor.h"
+
+@implementation RefreshMonitor
+
+@end
