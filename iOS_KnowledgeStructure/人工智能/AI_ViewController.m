5bfd22eb75b451762c583301d31f9f9dbe7f7179
周飞 <1174977961@qq.com>
2019-03-09 16:24:50 +0800

    feature:完善AI功能

--- iOS_KnowledgeStructure/人工智能/AI_ViewController.m
+++ iOS_KnowledgeStructure/人工智能/AI_ViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "AI_ViewController.h"
+#import <Vision/Vision.h>
+#import "Resnet50.h"
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <CoreFoundation/CoreFoundation.h>
 
-@interface AI_ViewController ()
-
+@interface AI_ViewController () <UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic, strong) UIImagePickerController *imagePickerController;
+@property (weak, nonatomic) IBOutlet UILabel *photoNameLabel;
 @end
 
 @implementation AI_ViewController
@@ -19,14 +25,95 @@
     // Do any additional setup after loading the view from its nib.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    self.imagePickerController = [[UIImagePickerController alloc] init];
+    self.imagePickerController.delegate = self;
+    self.imagePickerController.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    self.imagePickerController.allowsEditing = YES;
+    self.imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+    self.imagePickerController.mediaTypes = @[(NSString *)kUTTypeImage];
+    self.imagePickerController.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+    [self.navigationController presentViewController:self.imagePickerController
+                                            animated:YES
+                                          completion:nil];
 }
-*/
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    NSString *mediaType=[info objectForKey:UIImagePickerControllerMediaType];
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]){
+        CGSize thesize = CGSizeMake(224, 224);
+        UIImage *theimage = [self image:info[UIImagePickerControllerEditedImage] scaleToSize:thesize];
+        self.imageView.image = theimage;
+        
+        CVPixelBufferRef imageRef = [self pixelBufferFromCGImage:theimage.CGImage];
+        Resnet50 *resnet50Model = [[Resnet50 alloc] init];
+        NSError *error = nil;
+        Resnet50Output *output = [resnet50Model predictionFromImage:imageRef
+                                                              error:&error];
+        if (error == nil) {
+            self.photoNameLabel.text = output.classLabel;
+        } else {
+            NSLog(@"Error is %@", error.localizedDescription);
+        }
+    }
+    
+    UIImagePickerController *imagePickerVC = picker;
+    [imagePickerVC dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+- (CVPixelBufferRef) pixelBufferFromCGImage: (CGImageRef) image {
+    NSDictionary *options = @{
+                              (NSString*)kCVPixelBufferCGImageCompatibilityKey : @YES,
+                              (NSString*)kCVPixelBufferCGBitmapContextCompatibilityKey : @YES,
+                              };
+    
+    CVPixelBufferRef pxbuffer = NULL;
+    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault, CGImageGetWidth(image),
+                                          CGImageGetHeight(image), kCVPixelFormatType_32ARGB, (__bridge CFDictionaryRef) options,
+                                          &pxbuffer);
+    if (status!=kCVReturnSuccess) {
+        NSLog(@"Operation failed");
+    }
+    NSParameterAssert(status == kCVReturnSuccess && pxbuffer != NULL);
+    
+    CVPixelBufferLockBaseAddress(pxbuffer, 0);
+    void *pxdata = CVPixelBufferGetBaseAddress(pxbuffer);
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pxdata, CGImageGetWidth(image),
+                                                 CGImageGetHeight(image), 8, 4*CGImageGetWidth(image), rgbColorSpace,
+                                                 kCGImageAlphaNoneSkipFirst);
+    NSParameterAssert(context);
+    
+    CGContextConcatCTM(context, CGAffineTransformMakeRotation(0));
+    CGAffineTransform flipVertical = CGAffineTransformMake( 1, 0, 0, -1, 0, CGImageGetHeight(image) );
+    CGContextConcatCTM(context, flipVertical);
+    CGAffineTransform flipHorizontal = CGAffineTransformMake( -1.0, 0.0, 0.0, 1.0, CGImageGetWidth(image), 0.0 );
+    CGContextConcatCTM(context, flipHorizontal);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(image),
+                                           CGImageGetHeight(image)), image);
+    CGColorSpaceRelease(rgbColorSpace);
+    CGContextRelease(context);
+    
+    CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
+    return pxbuffer;
+}
+
+- (UIImage*)image:(UIImage *)image scaleToSize:(CGSize)size{
+    
+    UIGraphicsBeginImageContext(size);
+    
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    
+    UIImage* scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return scaledImage;
+}
 
 @end
