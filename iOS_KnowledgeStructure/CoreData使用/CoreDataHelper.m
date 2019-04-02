76befd12365def3b0b7d8cfadd3ffbf20f75d21a
周飞 <1174977961@qq.com>
2019-04-03 00:13:06 +0800

    update: 新增数据库存储方法

--- iOS_KnowledgeStructure/CoreData使用/CoreDataHelper.m
+++ iOS_KnowledgeStructure/CoreData使用/CoreDataHelper.m
@@ -10,7 +10,7 @@
 #import <CoreData/CoreData.h>
 
 @implementation CoreDataHelper
-- (void)setupCoreData {
+- (NSManagedObjectContext *)managedObjectContext {
     //加载模型文件
     NSManagedObjectModel *modelFile = [NSManagedObjectModel mergedModelFromBundles:nil];
 
@@ -21,6 +21,38 @@
     NSString *documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
     NSURL *url = [NSURL URLWithString:[documentDir stringByAppendingPathComponent:@"coreData.data"]];
     
+    //添加数据库
+    NSError *error = nil;
+    NSPersistentStore *store = [coor addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:url options:nil error:&error];
+    if (store == nil) {
+        [NSException raise:@"添加数据库失败" format:@"%@",[error description]];
+    }
+    
+    NSManagedObjectContext *context = [[NSManagedObjectContext alloc] init];
+    context.persistentStoreCoordinator = coor;
+    
+    return context;
+}
+
+- (void)saveData2DB {
+    NSManagedObjectContext *context = [self managedObjectContext];
+    //person
+    NSManagedObject *person = [NSEntityDescription insertNewObjectForEntityForName:@"Person" inManagedObjectContext:context];
+    [person setValue:@"jame" forKey:@"name"];
+    [person setValue:@(20) forKey:@"age"];
+    
+    //card
+    NSManagedObject *card = [NSEntityDescription insertNewObjectForEntityForName:@"Card" inManagedObjectContext:context];
+    [card setValue:@(123) forKey:@"num"];
+    
+    //建立关系
+    [person setValue:card forKey:@"card"];
+    
+    NSError *error;
+    BOOL result = [context save:&error];
+    if (!result) {
+        [NSException raise:@"数据库访问错误" format:@"%@",[error description]];
+    }
 }
 
 
