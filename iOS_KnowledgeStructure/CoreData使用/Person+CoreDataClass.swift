e3c046a69fced999c893bae551e632ab1231c547
周飞 <1174977961@qq.com>
2019-04-03 23:56:13 +0800

    add: 添加manageObject

---
+++ iOS_KnowledgeStructure/CoreData使用/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  iOS_KnowledgeStructure
+//
+//  Created by 周飞 on 2019/4/3.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
