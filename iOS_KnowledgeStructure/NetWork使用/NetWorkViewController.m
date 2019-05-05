0e0a1346a269cfe8d7e21827ad16ad86022fd207
周飞 <1174977961@qq.com>
2019-05-05 23:45:47 +0800

    add: 添加注释

--- iOS_KnowledgeStructure/NetWork使用/NetWorkViewController.m
+++ iOS_KnowledgeStructure/NetWork使用/NetWorkViewController.m
@@ -39,21 +39,23 @@
 }
 
 - (void)setupData {
-    [NSURLCache sharedURLCache];
+    //缓存策略在Get方法中采用,是苹果提供的内存和本地缓存的综合工具
+    //设置自定义的缓存大小
+    NSURLCache *cache = [[NSURLCache alloc] initWithMemoryCapacity:5*1024*1024 diskCapacity:20*1024*1024 diskPath:nil];
+    [NSURLCache setSharedURLCache:cache];
     
     NSURL *url = [NSURL URLWithString:@"http://www.baidu.com"];
     //NSURLRequestUseProtocolCachePolicy:采用响应头中的"Cache-Control"字段对于的缓存策略
     //NSURLRequestReturnCacheDataElseLoad:使用本地采用的缓存方式
     //缓存数据以file.db的形式保存在 /Library/Caches/项目名下。
-    //缓存策略在Get方法中采用
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:60];
     NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         NSLog(@"response: %@",response);
     }];
-    self.task = task;
     
+    self.task = task;
     [task resume];
 }
 
