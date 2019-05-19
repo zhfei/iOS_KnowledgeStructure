d86a9bd0d4389830063b060fcc2ad892f580a274
周飞 <1174977961@qq.com>
2019-05-19 23:57:34 +0800

    add: 添加gcd timer

--- iOS_KnowledgeStructure/DispatchSource使用/DispatchSourceViewController.m
+++ iOS_KnowledgeStructure/DispatchSource使用/DispatchSourceViewController.m
@@ -13,20 +13,56 @@
 @end
 
 @implementation DispatchSourceViewController
-
+#pragma mark - Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self addUI];
+    [self setupUI];
+    [self setupLayout];
+    [self setupData];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Private Method
+- (void)addUI {
+    
 }
-*/
+
+- (void)setupUI {
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)setupLayout {
+    
+}
+
+- (void)setupData {
+    
+}
+
+//MARK：test1
+- (void)testGCDSource {
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, <#dispatchQueue#>);
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, <#intervalInSeconds#> * NSEC_PER_SEC, <#leewayInSeconds#> * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(timer, ^{
+        <#code to be executed when timer fires#>
+    });
+    dispatch_resume(timer);
+}
+
+// MARK: overwrite
+
+#pragma mark - Public Method
+
+#pragma mark - Event
+
+#pragma mark - Delegate
+
+#pragma mark - Getter, Setter
+
+#pragma mark - NSCopying
+
+#pragma mark - NSObject
+
 
 @end
