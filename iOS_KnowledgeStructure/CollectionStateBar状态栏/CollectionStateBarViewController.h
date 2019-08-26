ac4a8c91df887ff6c0bb71b9b50fa9629c58053c
周飞 <1174977961@qq.com>
2019-08-26 23:54:07 +0800

    add: 添加滚动栏

---
+++ iOS_KnowledgeStructure/CollectionStateBar状态栏/CollectionStateBarViewController.h
@@ -0,0 +1,17 @@
+//
+//  CollectionStateBarViewController.h
+//  iOS_KnowledgeStructure
+//
+//  Created by 周飞 on 2019/8/26.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CollectionStateBarViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
