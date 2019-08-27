09b9697c66ff4bf1a65b0b2ed2926716578b5291
zhoufei <1174977961@qq.com>
2019-08-27 21:15:24 +0800

    other: 更新CollectionStateBar

---
+++ iOS_KnowledgeStructure/CollectionStateBar状态栏/CollectionStateBarViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CollectionStateBarViewController.swift
+//  iOS_KnowledgeStructure
+//
+//  Created by zhoufei on 2019/8/27.
+//  Copyright © 2019年 zhf. All rights reserved.
+//
+
+import UIKit
+
+class CollectionStateBarViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
