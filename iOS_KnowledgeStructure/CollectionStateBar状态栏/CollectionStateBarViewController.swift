589df1b98ea0c564c10c72833e71bb2b78c89749
周飞 <1174977961@qq.com>
2019-08-29 00:04:41 +0800

    other: update

--- iOS_KnowledgeStructure/CollectionStateBar状态栏/CollectionStateBarViewController.swift
+++ iOS_KnowledgeStructure/CollectionStateBar状态栏/CollectionStateBarViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class CollectionStateBarViewController: UIViewController {
     
+    var collectionView: UICollectionView?
+    let Identifier = "CollectionViewCell"
+    let headerIdentifier = "UICollectionElementKindSectionFooter"
+    let footIdentifier = "UICollectionElementKindSectionFooter"
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -18,11 +23,12 @@ class CollectionStateBarViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        setupUI()
     }
     
     func setupUI() {
         let widthS = UIScreen.main.bounds.size.width
+
         
         let layout = UICollectionViewFlowLayout.init()
         layout.itemSize = CGSize(width: 60, height: 60)
@@ -35,24 +41,46 @@ class CollectionStateBarViewController: UIViewController {
         layout.footerReferenceSize = CGSize.init(width: widthS, height: 80)
         
         
-        let frame = CGRect.init(x: 0, y: 100, width: 300, height: 100);
-        let collectV = UICollectionView.init(frame: frame, collectionViewLayout: layout);
-        collectV.delegate = self;
-        collectV.dataSource = self;
-        collectV.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell");
+        collectionView = UICollectionView.init(frame: CGRect(x:0, y:64, width:widthS, height:400), collectionViewLayout: layout)
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell");
+        self.view.addSubview(collectionView!)
+
+        
+        // 注册cell
+        collectionView?.register(UINib.init(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: Identifier)
+
     }
     
     
 }
 
 extension CollectionStateBarViewController: UICollectionViewDataSource,UICollectionViewDelegate {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10;
+        return 18
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath)
-        return cell;
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifier, for: indexPath)
+        cell.backgroundColor = armColor()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+    func armColor()->UIColor{
+        let red = CGFloat(arc4random()%256)/255.0
+        let green = CGFloat(arc4random()%256)/255.0
+        let blue = CGFloat(arc4random()%256)/255.0
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
 
 }
